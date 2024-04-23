d9537db7ee9276f94cc6ea490d4a786092979139
Jude <judejeevan.g1@gmail.com>
2024-04-23 22:50:30 +0530

    added the response headers using res.headers() added res.write(), which writes all the data to the resonse we can have multiple re.write() functions before we end it with res.end()

--- first-server/app.js
+++ first-server/app.js
@@ -1,7 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.headers, req.method)
+    // console.log(req.url, req.headers, req.method)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hello World</h1>')
+    res.end()
 })
 
 server.listen(3000)
